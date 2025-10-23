a3f932b51cbd9d58e0e71d640662c4628b2253ce
Ibne-Shahid <anasbinshahid2004@gmail.com>
2025-10-23 11:39:31 +0600

    banner design modified

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,12 +5,12 @@ import { Link } from 'react-router'
 
 const Hero = () => {
   return (
-    <div className='bg-base-200 flex justify-center items-center py-5 px-5 md:px-10 md:py-10 lg:px-0 lg:py-0'>
-        <div className='md:w-3/7 hidden md:block'><img src={heroIcon} alt="" /></div>
-        <div className='md:w-4/7 text-center md:text-start'>
+    <div className='bg-base-200 flex md:flex-col lg:flex-row justify-center items-center py-5 px-5 md:px-10 md:py-10 lg:px-0 lg:py-0'>
+        <div className='lg:w-3/7 hidden md:block'><img src={heroIcon} alt="" /></div>
+        <div className='lg:w-4/7 text-center lg:text-start'>
             <h1 className='font-extrabold text-2xl md:text-4xl'>WELCOME</h1>
-            <p className='text-2xl md:text-6xl font-bold'>To <br className='block md:hidden'/> <span className='text-secondary text-4xl md:text-6xl'>FunVille <span className='text-primary'>Toys</span></span></p>
-            <p className='md:w-2/3 mt-5'>
+            <p className='text-2xl md:text-6xl font-bold'>To <br className='block lg:hidden'/> <span className='text-secondary text-4xl md:text-6xl'>FunVille <span className='text-primary'>Toys</span></span></p>
+            <p className='lg:w-2/3 mt-5'>
                 we bring smiles to every child’s face! Explore a colorful world of creativity, imagination, and endless playtime joy. From classic building sets and action figures to puzzles, dolls, and educational toys — we have something special for every kid (and the kid inside you!).
             </p>
             <Link to="/allToys"><button className='btn btn-accent mt-5 text-white'>Shop Now</button></Link>
